bbc9a4dd4ea617a8e97992a7033fd89b298a21db
Yü Li Chang Lin <40342792+Yuelichang@users.noreply.github.com>
2025-03-26 23:59:55 +0100

    again

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,12 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.sonarqube") version "6.0.1.5171"
-    id("jacoco")
+    id("org.sonarqube") version "5.1.0.4882"
 }
 
 android {
     namespace = "com.example.boomboomfrontend"
     compileSdk = 33
-    buildToolsVersion = "33.0.1"
 
     defaultConfig {
         applicationId = "com.example.boomboomfrontend"
@@ -35,7 +33,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-        isCoreLibraryDesugaringEnabled = true
     }
     kotlinOptions {
         jvmTarget = "17"
@@ -53,30 +50,6 @@ android {
     }
 }
 
-tasks.register("jacocoTestReport", JacocoReport::class) {
-    dependsOn("testDebugUnitTest")
-
-    reports {
-        xml.required.set(true)
-        html.required.set(true)
-    }
-
-    classDirectories.setFrom(
-        fileTree("${buildDir}/intermediates/javac/debug") {
-            include("**/classes/**")
-        },
-        fileTree("${buildDir}/tmp/kotlin-classes/debug")
-    )
-    sourceDirectories.setFrom(
-        files("src/main/java", "src/main/kotlin")
-    )
-    executionData.setFrom(
-        fileTree("${buildDir}/jacoco") {
-            include("testDebugUnitTest.exec")
-        }
-    )
-}
-
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
@@ -87,9 +60,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-
-    implementation("com.android.tools:desugar_jdk_libs:2.0.3")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -104,10 +74,5 @@ sonar {
         property("sonar.projectKey", "SE2-BOOM-BOOM-KITTENS_BOOOM-BOOM-FRONTEND")
         property("sonar.organization", "se2-boom-boom-kittens")
         property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.sources", "src/main/java,src/main/kotlin")
-        property("sonar.tests", "src/test/java,src/androidTest/java")
-        property("sonar.coverage.jacoco.xmlReportPaths", "${buildDir}/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
-        property("sonar.language", "kotlin")
-        property("sonar.java.binaries", "${buildDir}/classes")
     }
 }
